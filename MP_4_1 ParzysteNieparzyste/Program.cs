ced28b9fdc02fb02dfa1c0a60746c481eb070258
Kamvitae Surimushado <kamvitae@gmail.com>
2020-09-23 21:48:28 +0200

    project upload

---
+++ MP_4_1 ParzysteNieparzyste/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MP_4_1_ParzysteNieparzyste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string request = $"Proszę wprowadzić liczbę całkowitą w przedziale {Int32.MinValue} a {Int32.MaxValue}\n";
+            Console.WriteLine(request);
+            
+            while (true)
+            {
+                var userInput = Console.ReadLine();
+                bool success = int.TryParse(userInput , out int result);
+
+                if (success)
+                {
+                    int number = int.Parse(userInput);
+                    if (number % 2!= 0 || number == 0) 
+                    Console.WriteLine("Jest to liczba nieparzysta");
+                    else if(number % 2==0)
+                    Console.WriteLine("Jest to liczba parzysta");
+                }
+                else
+                {
+                    Console.WriteLine(request);
+                }
+            } 
+        }
+    }
+}
